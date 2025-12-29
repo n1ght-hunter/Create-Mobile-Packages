42bf256fcc54dae9c572fc9c57d86a5f1cc3a89c
Night_Hunter <samuelhuntnz@gmail.com>
2025-12-29 19:51:35 +1300

    Add server event handling for GlobalDronePortTracker and register AdvancedBeePort capabilities

--- src/main/java/de/theidler/create_mobile_packages/CommonEvents.java
+++ src/main/java/de/theidler/create_mobile_packages/CommonEvents.java
@@ -1,6 +1,8 @@
 package de.theidler.create_mobile_packages;
 
+import de.theidler.create_mobile_packages.blocks.advanced_bee_port.AdvancedBeePortBlockEntity;
 import de.theidler.create_mobile_packages.blocks.bee_port.BeePortBlockEntity;
+import de.theidler.create_mobile_packages.blocks.bee_port.GlobalDronePortTracker;
 import de.theidler.create_mobile_packages.entities.robo_entity.RoboEntity;
 import de.theidler.create_mobile_packages.robo.RoboManager;
 import net.minecraft.server.level.ServerLevel;
@@ -8,6 +10,8 @@ import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityTravelToDimensionEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.event.server.ServerStoppingEvent;
 import net.neoforged.neoforge.event.tick.LevelTickEvent;
 
 
@@ -28,6 +32,7 @@ public class CommonEvents {
         @SubscribeEvent
         public static void registerCapabilities(net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent event) {
             BeePortBlockEntity.registerCapabilities(event);
+            AdvancedBeePortBlockEntity.registerCapabilities(event);
         }
     }
 
@@ -37,6 +42,16 @@ public class CommonEvents {
         if (!(event.getEntity() instanceof RoboEntity)) return;
 
         event.setCanceled(true);
+    }
+
+    @SubscribeEvent
+    public static void onServerStarting(ServerStartingEvent event) {
+        GlobalDronePortTracker.setServer(event.getServer());
+    }
+
+    @SubscribeEvent
+    public static void onServerStopping(ServerStoppingEvent event) {
+        GlobalDronePortTracker.clear();
     }
 
 }
